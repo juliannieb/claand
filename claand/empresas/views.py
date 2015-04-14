3a380d50b26954bcfad08f8b781bad7bdd685b46
juliannieb <juliannieb@gmail.com>
2015-04-13 22:01:04 -0500

    Agrega form de registrar empresa ahora si bien.

--- claand/empresas/views.py
+++ claand/empresas/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
-from empresas.forms import EmpresaForm, DireccionForm
+from empresas.forms import EmpresaForm, DireccionForm, NumeroTelefonicoForm, RedSocialForm
 from empresas.models import Empresa, Direccion, EmpresaTieneDireccion
 
 """
@@ -33,13 +33,25 @@ def registrar_empresa(request):
     if request.method == 'POST':
         form = EmpresaForm(request.POST)
         formDireccion = DireccionForm(request.POST)
+        formNumeroTelefonico = NumeroTelefonicoForm(request.POST)
+        formRedSocial = RedSocialForm(request.POST)
 
         # Have we been provided with a valid form?
         if form.is_valid() and formDireccion.is_valid():
             # Save the new category to the database.
-            direccion = formDireccion.save(commit=True)
             empresa = form.save(commit=True)
+            direccion = formDireccion.save(commit=True)
+
             EmpresaTieneDireccion(empresa=empresa, direccion=direccion).save()
+
+            if formNumeroTelefonico.has_changed() and formNumeroTelefonico.is_valid():
+                numero_telefonico = formNumeroTelefonico.instance
+                numero_telefonico.empresa = empresa
+                numero_telefonico.save()
+            if formRedSocial.has_changed() and formRedSocial.is_valid():
+                red_social = formRedSocial.instance
+                red_social.empresa = empresa
+                red_social.save()
 
             # Now call the index() view.
             # The user will be shown the homepage.
@@ -47,11 +59,17 @@ def registrar_empresa(request):
         else:
             # The supplied form contained errors - just print them to the terminal.
             print (form.errors)
+            print (formDireccion.errors)
+            print (formNumeroTelefonico.errors)
+            print (formRedSocial.errors)
     else:
         # If the request was not a POST, display the form to enter details.
         form = EmpresaForm()
         formDireccion = DireccionForm()
+        formNumeroTelefonico = NumeroTelefonicoForm()
+        formRedSocial = RedSocialForm()
+        context = {'form':form, 'formDireccion':formDireccion, 'formNumeroTelefonico':formNumeroTelefonico, 'formRedSocial':formRedSocial}
 
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
-    return render(request, 'empresas/registrar_empresa2.html', {'form':form, 'formDireccion':formDireccion})
+    return render(request, 'empresas/registrar_empresa2.html', context)
