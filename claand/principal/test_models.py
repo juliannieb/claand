b7cf98d100c2d7fd240d551e4100d5d1195bc0b5
csssaz <alecassis16@gmail.com>
2015-04-27 00:19:38 -0500

    test creacion vendedor

---
+++ claand/principal/test_models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User, Group
+
+from principal.models import Vendedor
+
+class ModelosPrincipal(TestCase):
+	fixtures = ['contactos.json', 'empresas.json', 'cotizaciones.json', 'principal.json', 'users.json']
+
+	def setUp(self):
+		self.usuario = User.objects.create_user('temporary', 'temporary@gmail.com', 'temporary')
+
+	def tearDown(self):
+		pass
+
+	def test_save_vendedor(self):
+		""" test para probar la creación de un vendedor
+		"""
+		vendedor_prueba = Vendedor.objects.create(user=self.usuario)
+		self.assertIsNotNone(vendedor_prueba.user.groups.get(name='vendedor'))
+
+
+
+
+
