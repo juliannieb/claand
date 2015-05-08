7c1e112c0bcbcafe39529bc96aaaf04d68e5f23b
csssaz <alecassis16@gmail.com>
2015-05-07 20:26:49 -0500

    settings para deploy

--- claand/claand/settings.py
+++ claand/claand/settings.py
@@ -15,14 +15,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tsdx)2ig2869w$)fhgb6ohusy*bqvm-q&63efvkkivu!c(t^=0'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.55.233.163']
 
 
 # Application definition
@@ -65,8 +65,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "prueba1",
-        "USER": "prueba",
-        "PASSWORD": "hola",
+        "USER": os.environ['DB_USER'],
+        "PASSWORD": os.environ['DB_PASSWORD'],
         "HOST": "localhost",
         "PORT": "",
     }
